6ba8dfa073a840cfe2fe7f7cf98383677fb20140
Permata NMR <permata.nmr@pmbs.ac.id>
2025-03-24 09:24:10 +0700

    add admin customuser

--- evaluator/admin.py
+++ evaluator/admin.py
@@ -2,10 +2,5 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-# class CustomUserAdmin(UserAdmin):
-#     model = CustomUser
-    # list_display = ('email', 'full_name', 'role', 'is_staff', 'is_active')
-    # fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('role',)}),)
-    # add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('role',)}),)
+admin.site.register(CustomUser)
 
-# admin.site.register(CustomUser, CustomUserAdmin)
